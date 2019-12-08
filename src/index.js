002894c5a7dff9e7428b5ca81cabdce26a08638d
Gene Mecija <genemecija@outlook.com>
2019-12-08 11:34:47 -0800

    Double to single quotes

--- src/index.js
+++ src/index.js
@@ -1,60 +1,61 @@
-import List from "./list";
-import Task from "./task";
-import displayTasksForList from "./listMethods";
-import showLists from "./appNav";
+/* eslint-disable comma-dangle */
+import List from './list';
+import Task from './task';
+import displayTasksForList from './listMethods';
+import showLists from './appNav';
 
-window.document.title = 'To-Do-List - Gene Mecija'
+window.document.title = 'To-Do-List - Gene Mecija';
 
 // Setup example lists and tasks
-let firstList = new List("Become Rich");
-let task1 = new Task(
+const firstList = new List('Become Rich');
+const task1 = new Task(
   firstList.id,
-  "Dig for gold",
-  "There must be some gold left. Better start digging",
-  "2019-12-01",
-  "Money"
+  'Dig for gold',
+  'There must be some gold left. Better start digging',
+  '2019-12-01',
+  'Money'
 );
 firstList.taskList[task1.id] = task1;
-let task2 = new Task(
+const task2 = new Task(
   firstList.id,
-  "Find change",
-  "Look in couch cushions for loose change",
-  "2019-11-27",
-  "Money"
+  'Find change',
+  'Look in couch cushions for loose change',
+  '2019-11-27',
+  'Money'
 );
 firstList.taskList[task2.id] = task2;
 
-let secondList = new List("Eat Healthy");
-let task3 = new Task(
+const secondList = new List('Eat Healthy');
+const task3 = new Task(
   secondList.id,
-  "Buy Vegetables",
-  "Consider buying vegetables instead of ice cream",
-  "2023-01-01",
-  "Food"
+  'Buy Vegetables',
+  'Consider buying vegetables instead of ice cream',
+  '2023-01-01',
+  'Food'
 );
 secondList.taskList[task3.id] = task3;
 
 // Load examples into localStorage
-localStorage.setItem("allLists", JSON.stringify([firstList, secondList]));
+localStorage.setItem('allLists', JSON.stringify([firstList, secondList]));
 
 // Set up app layout
-let body = document.querySelector("body");
+const body = document.querySelector('body');
 
-let appWindow = document.createElement("div");
-appWindow.classList.add("appWindow");
+const appWindow = document.createElement('div');
+appWindow.classList.add('appWindow');
 body.appendChild(appWindow);
 
-let appNav = document.createElement("div");
-appNav.classList.add("appNav");
+const appNav = document.createElement('div');
+appNav.classList.add('appNav');
 appWindow.appendChild(appNav);
 
-let appContent = document.createElement("div");
-appContent.classList.add("appContent");
+const appContent = document.createElement('div');
+appContent.classList.add('appContent');
 appWindow.appendChild(appContent);
 
 // Get localStorage data
-var allLists = [];
-allLists = JSON.parse(localStorage.getItem("allLists"));
+let allLists = [];
+allLists = JSON.parse(localStorage.getItem('allLists'));
 
 // Populate appNav with lists
 appNav.appendChild(showLists());
